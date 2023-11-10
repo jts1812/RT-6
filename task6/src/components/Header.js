305fdfabb6ec28205844bba187631c70bc6c2ab9
jts1812 <144221298+jts1812@users.noreply.github.com>
2023-11-10 23:01:40 +0530

    Add files via upload

---
+++ task6/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Sankar</h1>
+      <p>ui/ux Developer</p>
+    </header>
+  );
+}
+
+export default Header;
